f89fc6caafd709a3c70de3af1d861f9398301ed9
maks7667 <156918193+maks7667@users.noreply.github.com>
2024-04-17 03:12:08 +0600

    Update last_hw_1.py

--- last_hw_1.py
+++ last_hw_1.py
@@ -1,11 +1,21 @@
+"""
+Получает текст от пользователя и
+создаёт word-файл с этим текстом
+"""
 from docx import Document
 
-def create_word_file(text):
+def create_word(text: any ) -> None:
+    """
+    Создаёт word-файл
+    """
     doc = Document()
     doc.add_paragraph(text)
     doc.save("output.docx")
     print("Word-файл успешно создан.")
 
 if __name__ == "__main__":
+    """
+    Получает текст от пользователя
+    """
     user_input = input("Введите текст для создания Word-файла: ")
-    create_word_file(user_input)
+    create_word(user_input)
